442663f0b6e1892a49642b539c7dc00b7271f401
Iman <rai.dilungchha@live.com>
2020-05-20 23:11:54 +0100

    Added camera follow to the car.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform car;
+    public Vector3 offset;
+    private Space offsetPS = Space.Self;
+    //public float smooth = 0.5f;
+    private bool lookAt = true; 
+   
+    void FixedUpdate()
+    {
+        /*
+        Vector3 newPosition = car.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, newPosition, smooth);
+        transform.position = smoothPosition;
+        */
+        if (offsetPS == Space.Self)
+        {
+            transform.position = car.TransformPoint(offset);
+        }
+        else
+        {
+            transform.position = car.position + offset;
+        }
+
+        if (lookAt)
+        {
+            transform.LookAt(car);
+        }
+        else
+        {
+            transform.rotation = car.rotation;
+        }
+    }
+}
