9779bfdd896599e6b180403cca41e674dd29c6fb
Iman <rai.dilungchha@live.com>
2020-06-05 19:23:32 +0100

    Added Main Menu Scene

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SelectTrack1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void SelectTrack2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+}
